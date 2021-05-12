f933e5183b2bcdc360c09130f51a6a0cefbd00ac
MANISH KUMAR <69623308+manu1979522@users.noreply.github.com>
2021-05-12 11:10:08 +0530

    Delete migrate.sql

--- migrate.sql
+++
@@ -1,7 +0,0 @@
-    IF EXISTS ( SELECT  1
-                FROM    information_schema.Tables
-                WHERE   table_schema = 'testDB'
-                        AND TABLE_NAME = 'EMP' )
-        PRINT 'the table exists' 
-    ELSE
-        PRINT 'The table isn''t there'
